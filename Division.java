f5b73328d97d63b7ff24d9bb256c0627ee3d7ad8
Gayatri-stack123 <gayatrichikkodi80@gmail.com>
2025-05-06 10:51:24 +0530

    Update Division.java
    save

--- Division.java
+++ Division.java
@@ -12,16 +12,3 @@ public class Division {
         }  
     }
 }
-
-public class Division {
-    public static void main(String[] args) {
-        int a=15;
-        int b=0;
-        try{
-            int div =a/b;
-            System.out.println("Division : "+div);
-        }catch (ArithmeticException e){
-            System.out.println("Error : Cannont divisible by zero");
-        }  
-    }
-}
